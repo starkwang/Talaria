b7d8234b8a147a5f8339ab4d3c0f5918dcd7646b
starkwang <381152119@qq.com>
2016-09-03 17:57:08 +0800

    修复路径

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use('/js', express.static('build/js'));
-app.use('/css', express.static('build/css'));
-app.use('/img', express.static('static/img'));
-app.use('/lib', express.static('static/lib'));
-
-
+app.use('/build', express.static('build'));
+app.use('/static', express.static('static'));
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/index.html');
